6f4e505c55ebe0abf4cd54775c40f6e22b21e28f
Matrix3600 <matrix3600x@gmail.com>
2023-04-10 13:29:46 +0200

    Update script to extract msvcrt*.lib files from WDK iso

--- msvc/extract_msvcrt_libs.cmd
+++ msvc/extract_msvcrt_libs.cmd
@@ -28,29 +28,22 @@ set "exit_code=10"
 if not exist "%wdk_filename%" if exist "%wdk_filename2%" set "wdk_filename=%wdk_filename2%"
 set "seven_zip_dir2=C:\Program Files (x86)\7-Zip"
 if not exist "%seven_zip_dir%\7z.exe" if exist "%seven_zip_dir2%\7z.exe" (
-set "seven_zip_dir=%seven_zip_dir2%"
+	set "seven_zip_dir=%seven_zip_dir2%"
 )
 set "seven_zip=%seven_zip_dir%\7z.exe"
 
 echo.
 for %%# in ("%wdk_filename%" "%seven_zip%" "%seven_zip_dir%\7z.dll") do if not exist "%%~#" (
-echo Required file "%%~#" is missing.
-set "exit_code=1"
-goto end
+	echo Required file "%%~#" is missing.
+	set "exit_code=1"
+	goto end
 )
 
-echo Checking "%wdk_filename%" ...
-for /f "delims=" %%h in (
-'certutil -hashfile "%wdk_filename%" SHA256 ^| find /v "SHA256" ^| find /v "CertUtil"'
-) do set "hash=%%h"
-set "hash=%hash: =%"
-if not "%hash%"=="%wdk_sha256%" (
-echo SHA256 hash does not match for "%wdk_filename%":
-echo "%hash%"
-echo instead of:
-echo "%wdk_sha256%"
-set "exit_code=2"
-goto end
+:: Check WDK file
+call :check_file "%wdk_filename%" "%wdk_sha256%"
+if errorlevel 1 (
+	set "exit_code=2"
+	goto end
 )
 echo.
 
@@ -63,6 +56,7 @@ echo Extracting %lib_x86% ...
 "%seven_zip%" e -aoa "%cab_x86%" _msvcrt.lib_00025 >nul
 if errorlevel 1 goto error
 if exist "%lib_x86%" del "%lib_x86%"
+if exist "%lib_x86%" goto error
 ren _msvcrt.lib_00025 "%lib_x86%"
 if errorlevel 1 goto error
 del "%cab_x86%" >nul 2>&1
@@ -72,6 +66,7 @@ echo Extracting %lib_x64% ...
 "%seven_zip%" e -aoa "%cab_x64%" _msvcrt.lib_00024 >nul
 if errorlevel 1 goto error
 if exist "%lib_x64%" del "%lib_x64%"
+if exist "%lib_x64%" goto error
 ren _msvcrt.lib_00024 "%lib_x64%"
 if errorlevel 1 goto error
 del "%cab_x64%" >nul 2>&1
@@ -88,3 +83,37 @@ exit /b %exit_code%
 :error
 echo An error has occurred.
 goto end
+
+
+::
+:: Read the file and compute the hash.
+::
+:check_file
+setlocal
+set "file=%~1"
+set "file_hash=%~2"
+echo Checking "%file%" ...
+set "hash="
+for /f "skip=1 delims=" %%h in (
+	'certutil -hashfile "%file%" SHA256'
+	) do if not defined hash set "hash=%%h"
+
+:: CertUtil start error
+if not defined hash (
+	echo An error has occurred.
+	exit /b 3
+)
+echo.%hash% | find /i "CertUtil" >nul
+if %errorlevel% equ 0 (
+	echo FAILED: Unable to open/read the file.
+	exit /b 2
+)
+set "hash=%hash: =%"
+if not "%hash%"=="%file_hash%" (
+	echo SHA256 hash does not match:
+	echo "%hash%"
+	echo instead of:
+	echo "%file_hash%"
+	exit /b 1
+)
+exit /b 0
