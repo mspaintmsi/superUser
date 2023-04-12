16ae893bae52e95940d568bc0940d1c50c6ef41a
Matrix3600 <matrix3600x@gmail.com>
2023-04-12 17:30:49 +0200

    Update script to check hash of downloaded binaries

--- msvc/check_sha256.cmd
+++ msvc/check_sha256.cmd
@@ -1,6 +1,6 @@
 @echo off
 ::
-:: Checks the SHA256 hash for the files listed in the SHA256SUMS file.
+:: Check the SHA256 hash for the files listed in the SHA256SUMS file.
 ::
 :: Missing files are ignored.
 ::
@@ -11,86 +11,151 @@
 :: The "*" means "binary mode", the only supported by this script (as opposed
 :: to "text mode").
 ::
-:: Equivalent to GNU "sha256sum -c --ignore-missing SHA256SUMS"
+:: Equivalent to GNU "sha256sum -c --ignore-missing --warn --strict SHA256SUMS"
+::
+:: Error codes:
+:: 	1:	Some lines of the hash file are incorrectly formatted.
+:: 	2:	Some files could not be open/read.
+:: 	3:	Some file hashes do not match.
+:: 	4:	The hash file cannot be found.
+:: 	5:	The hash file is empty.
+:: 	6:	The Windows utility "CertUtil.exe" could not be started.
 ::
 
 set "hash_file_name=SHA256SUMS"
 
 setlocal EnableDelayedExpansion
+set "err_prefix=%~n0:"
+set "interactive="
+echo %cmdcmdline%| find /i "%~0" >nul
+if not errorlevel 1 set "interactive=1"
 
 echo(
 if not exist "%hash_file_name%" (
-	echo Hash file cannot be found ^("%hash_file_name%"^).
-	set "exit_code=3"
-	goto end
-)
-
-set "files_found=0"
-set "read_errors=0"
-set "failures=0"
-set "line_number=0"
-
-for /f "usebackq tokens=1,2" %%i in ("%hash_file_name%") do (
-	set /a line_number += 1
-	set "param2=%%~j"
-
-	:: Check first char of second parameter (* = binary mode)
-	if not "!param2:~0,1!"=="*" (
-		echo Missing binary indicator "*" at line !line_number!, hash file "%hash_file_name%".
-		echo Text mode is not supported by this script.
-		set "exit_code=5"
-		goto end
-	)
-	:: Remove first char "*"
-	set "filename=!param2:~1!"
-
-	if "!filename!"=="" (
-		echo Missing filename at line !line_number!, hash file "%hash_file_name%".
-		set "exit_code=6"
-		goto end
-	)
-	if exist "!filename!" (
-		set /a files_found += 1
-
-		:: Check the file hash
-		call :check_file "!filename!" "%%~i"
-		if errorlevel 3 (
-			set "exit_code=7"
-			goto end
-		) else if errorlevel 2 (set /a read_errors += 1
-		) else if errorlevel 1 set /a failures += 1
-	)
-)
-
-if %line_number% equ 0 (
-	echo Hash file is empty ^("%hash_file_name%"^).
+	echo %err_prefix% Hash file cannot be found ^("%hash_file_name%"^).>&2
 	set "exit_code=4"
 	goto end
 )
 
+set "files_found=0"
+set "line_format_errors=0"
+set "read_errors=0"
+set "failures=0"
+set "line_number=0"
+
+:: Read hash file line by line
+for /f "usebackq tokens=1*" %%i in ("%hash_file_name%") do (
+	set /a line_number += 1
+
+	set "file_hash=%%~i"
+	set "token2=%%~j"
+
+	:: Check line format and extract filename
+	call :check_line_format
+	if errorlevel 1 (
+		set /a line_format_errors += 1
+		echo %err_prefix% !error_msg! at line !line_number!, file "%hash_file_name%".>&2
+	) else (
+		:: Missing files are ignored
+		if exist "!filename!" (
+			set /a files_found += 1
+
+			:: Read the file, compute and compare the hash
+			call :check_file "!filename!" "!file_hash!"
+			if errorlevel 3 (
+				set "exit_code=6"
+				goto end
+			) else if errorlevel 2 (set /a failures += 1
+			) else if errorlevel 1 set /a read_errors += 1
+		)
+	)
+)
+
+if %line_number% equ 0 (
+	echo %err_prefix% Hash file is empty ^("%hash_file_name%"^).>&2
+	set "exit_code=5"
+	goto end
+)
+
 set "exit_code=0"
+
+:: Display information or warning messages
+
+: No files to check were found
 if %files_found% equ 0 (
-	echo No files found.
+	echo %err_prefix% No files found.>&2
 ) else (
-	if not "%read_errors%%failures%"=="00" echo(
-	if %read_errors% neq 0 (
-		echo WARNING: %read_errors% file^(s^) could not be open/read.
+	if not "%line_format_errors%%read_errors%%failures%"=="000" echo(>&2
+
+	:: Some lines are incorrectly formatted
+	if %line_format_errors% neq 0 (
+		if %line_format_errors% equ 1 (set "w=line is") else set "w=lines are"
+		echo %err_prefix% WARNING: %line_format_errors% !w! incorrectly formatted.>&2
 		set "exit_code=1"
 	)
-	if %failures% neq 0 (
-		echo WARNING: %failures% computed checksum^(s^) did NOT match.
+	:: Some files could not be open/read
+	if %read_errors% neq 0 (
+		if %read_errors% equ 1 (set "w=file") else set "w=files"
+		echo %err_prefix% WARNING: %read_errors% !w! could not be open/read.>&2
 		set "exit_code=2"
+	)
+	:: Some file hashes did not match
+	if %failures% neq 0 (
+		if %failures% equ 1 (set "w=checksum") else set "w=checksums"
+		echo %err_prefix% WARNING: %failures% computed !w! did NOT match.>&2
+		set "exit_code=3"
 	)
 )
 
 :end
-echo(
-pause
+if defined interactive (
+	echo(
+	pause
+)
 exit /b %exit_code%
 
 
 ::
-:: Read the file and compute the hash.
+:: check_line_format
+::
+:: Check line format of hash file and extract filename.
+::
+:: Input: file_hash, token2
+:: Output: filename, error_msg
+::
+:check_line_format
+
+:: Is this a valid SHA256 hash?
+set "err_pos="
+call :check_hash "%file_hash%" err_pos
+if errorlevel 1 (
+	set "error_msg=Invalid SHA256 hash"
+	if defined err_pos set "error_msg=!error_msg!, position %err_pos%"
+	exit /b 1
+)
+
+:: Check first char of second token (* = binary mode)
+if not "%token2:~0,1%"=="*" (
+	:: The text mode is not supported by this script
+	set "error_msg=No binary indicator "*""
+	exit /b 2
+)
+:: Remove first char "*"
+set "filename=%token2:~1%"
+:: Remove leading and trailing spaces
+call :trim filename
+
+if "%filename%"=="" (
+	set "error_msg=Missing filename"
+	exit /b 3
+)
+exit /b 0
+
+
+::
+:: check_file
+::
+:: Read the file, compute and compare the hash.
 ::
 :check_file
 setlocal
@@ -99,7 +164,7 @@ set "file_hash=%~2"
 echo Checking "%file%" ...
 set "hash="
 :: Empty file
-for /f %%f in ("%file%") do if %%~zf equ 0 (
+for /f "delims=" %%f in ("%file%") do if %%~zf equ 0 (
 	set "hash=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 )
 if not defined hash (
@@ -107,20 +172,61 @@ if not defined hash (
 		'certutil -hashfile "%file%" SHA256'
 		) do if not defined hash set "hash=%%h"
 )
-:: CertUtil start error
+:: CertUtil launch error
 if not defined hash (
-	echo An error has occurred.
+	echo %err_prefix% An error has occurred.>&2
 	exit /b 3
 )
 echo(%hash%| find /i "CertUtil" >nul
 if %errorlevel% equ 0 (
 	echo FAILED: Unable to open/read the file.
-	exit /b 2
+	exit /b 1
 )
 set "hash=%hash: =%"
 if /i not "%hash%"=="%file_hash%" (
 	echo FAILED
-	exit /b 1
+	exit /b 2
 )
 echo OK
 exit /b 0
+
+
+::
+:: check_hash <hash> <pos_var_name>
+::
+:: Check if <hash> is a valid SHA256 hash.
+:: In case of error, its position is returned in the <pos_var_name> variable.
+::
+:check_hash
+setlocal EnableDelayedExpansion
+set "hash_length=64"
+set "str=%~1"
+for /l %%i in (0,1,%hash_length%) do (
+	set "c=!str:~%%i,1!"
+	if "!c!"=="" set "p=%%i" & goto _ch_endloop
+	set /a "d=0x!c!" 2>nul
+	if errorlevel 1 set "p=%%i" & goto _ch_error
+)
+set "p=%hash_length%"
+:_ch_error
+set /a p += 1
+endLocal & set "%~2=%p%"
+exit /b 1
+
+:_ch_endloop
+if %p% lss %hash_length% goto _ch_error
+exit /b 0
+
+
+::
+:: trim <var_name>
+:: Remove leading and trailing spaces in variable <var_name>
+::
+:trim
+setLocal
+call :trimSub %%%1%%
+endLocal & set "%~1=%tempvar%"
+exit /b
+:trimSub
+set "tempvar=%*"
+exit /b
